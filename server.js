47157b4eb05cbfbe5eb58b66bc7c0e4fc47f2d2d
Louis Ruocco <louisruocco1@gmail.com>
2021-06-06 19:43:12 +0100

    Session Store set up

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening at " + port));
 
+const config = {
+    host: process.env.HOST, 
+    user: process.env.USER, 
+    password: process.env.PASSWORD, 
+    database: process.env.DATABASE
+}
+
+const sessionStore = new MySQLStore(config);
+
 dotenv.config({path: "./.env"});
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -17,4 +26,5 @@ app.use(session({
     secret: process.env.SESS_SECRET, 
     resave: false, 
     saveUninitialized: false, 
+    session: sessionStore
 }))
