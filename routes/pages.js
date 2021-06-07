3ae5c213821a381be65c7c689f23b2928d65c26b
Louis Ruocco <louisruocco1@gmail.com>
2021-06-07 19:08:49 +0100

    Login and Register Routes Created

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const session = require("express-session");
-const dotenv = require("dotenv");
+const path = require("path");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    const { userId } = req.session;
+    res.sendFile(path.join(__dirname, "../public/landing.html"));
+});
+
+router.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+router.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/register.html"));
+});
+
+router.get("/home", (req, res) => {
+    res.render("home");
+});
 
 module.exports = router;
