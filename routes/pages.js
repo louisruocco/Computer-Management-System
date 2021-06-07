67efa85f7c852684f507323ddb3a03171de82d7f
Louis Ruocco <louisruocco1@gmail.com>
2021-06-07 19:13:04 +0100

    Home route and Middleware created

--- routes/pages.js
+++ routes/pages.js
@@ -1,22 +1,39 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
+const { nextTick } = require("process");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+const redirectLanding = (req, res, next) => {
+    if(!req.session.userId){
+        return res.redirect("/");
+    } else {
+        next();
+    }
+}
+
+const redirectHome = (req, res, next) => {
+    if(req.session.userId){
+        return res.redirect("/home");
+    } else {
+        next();
+    }
+}
+
+router.get("/", redirectHome, (req, res) => {
     const { userId } = req.session;
     res.sendFile(path.join(__dirname, "../public/landing.html"));
 });
 
-router.get("/login", (req, res) => {
+router.get("/login", redirectHome, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
-router.get("/register", (req, res) => {
+router.get("/register", redirectHome, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/register.html"));
 });
 
-router.get("/home", (req, res) => {
+router.get("/home", redirectLanding, (req, res) => {
     res.render("home");
 });
 
